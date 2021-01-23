a630662ed1a26e8351aae6f53f356d2264006638
Username do Github <Email do GitHub>
2021-01-23 10:44:40 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/calc/Calculadora.java
@@ -0,0 +1,35 @@
+package com.example.calc;
+
+import android.os.Bundle;
+
+import java.util.Stack;
+
+public class Calculadora {
+    Stack<Integer> pilha = new Stack<Integer>();
+
+    public void somar(){
+        int n1 = (Integer) pilha.pop();
+        int n2 = (Integer) pilha.pop();
+        int resultado = n1+n2;
+        pilha.push(resultado);
+    }
+    public void subtrair(){
+        int n1 = (Integer) pilha.pop();
+        int n2 = (Integer) pilha.pop();
+        int resultado = n1-n2;
+        pilha.push(resultado);
+    }
+    public void dividir(){
+        int n1 = (Integer) pilha.pop();
+        int n2 = (Integer) pilha.pop();
+        int resultado = n1/n2;
+        pilha.push(resultado);
+    }
+    public void multiplicar(){
+        int n1 = (Integer) pilha.pop();
+        int n2 = (Integer) pilha.pop();
+        int resultado = n1*n2;
+        pilha.push(resultado);
+    }
+
+}
---
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.calc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.calc.Calculadora;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class MainActivity extends AppCompatActivity {
+
+    Calculadora calculadora = new  Calculadora();
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void empilhar(View v){
+        try {
+            EditText numero = (EditText) findViewById(R.id.leitor);
+            calculadora.pilha.push(Integer.parseInt(numero.getText().toString()));
+            numero.setText("");
+            atualizar();
+        }catch (NumberFormatException e){
+            alert("Erro: Digite o valor a ser empilhado");
+        }
+
+
+    }
+
+    public void desempilhar(View v){
+        try {
+            calculadora.pilha.pop();
+            atualizar();
+        }catch (EmptyStackException e){
+            alert("Erro: Stack Vazio");
+        }
+
+    }
+    public void calcular(View v){
+        Button botao = (Button) v;
+
+        char valor =  botao.getText().charAt(0);
+
+        try {
+            if(valor == '/'){
+                calculadora.dividir();
+            }else if(valor == '*'){
+                calculadora.multiplicar();
+            }else if(valor == '+'){
+                calculadora.somar();
+            }else{
+                calculadora.subtrair();
+            }
+            atualizar();
+        }catch (EmptyStackException e) {
+            atualizar();
+            alert("Erro: Stack Vazio");
+
+        }catch (ArithmeticException e) {
+            atualizar();
+            alert("Erro: Divisão por 0 é impossivel");
+        }
+    }
+
+    public static Stack<Integer> inverter(Stack<Integer> pilha) {
+        Stack<Integer> inverso = new Stack<Integer>();
+        for (int i = pilha.size()-1; i >= 0; i--
+             ) {
+            inverso.push(pilha.get(i));
+        }
+        return inverso;
+    }
+    public void atualizar(){
+        alert("");
+        TextView viewStack = (TextView) findViewById(R.id.stack);
+        viewStack.setText(inverter(calculadora.pilha).toString());
+    }
+    public void alert(String msg){
+        TextView viewAlert = (TextView) findViewById(R.id.alert);
+        viewAlert.setText(msg);
+    }
+
+
+}
